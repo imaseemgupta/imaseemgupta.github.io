0b42f968976230d9703aa4ba52d464111b7f8d66
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2021-11-12 18:08:35 +0200

    Remove unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
-// import App from "next/app";
-import type { AppProps /*, AppContext */ } from "next/app";
 import Head from "next/head";
 import "styles/globals.css";
 import "styles/theme.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,17 +13,5 @@ function MyApp({ Component, pageProps }: AppProps) {
     </>
   );
 }
-
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext: AppContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-
-//   return { ...appProps }
-// }
 
 export default MyApp;
